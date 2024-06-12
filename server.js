98882b6b8bf858fd74dfe12f2b97307ca0dd169c
NYcity <mossei@nate.com>
2024-06-12 14:19:47 +0800

    5

--- server.js
+++ server.js
@@ -1,25 +1,12 @@
-const express = require('express');
-const http = require('http');
-const https = require('https');
 const fs = require('fs');
+const https = require('https');
+const express = require('express');
 
-const HTTP_PORT = 8080;
-const HTTPS_PORT = 8443;
+const app = express()
 
 const options = {
-    key: fs.readFileSync('/www/server/panel/plugin/mail_sys/cert/cocomarket.app/fullchain.pem'),
-    cert: fs.readFileSync('/www/server/panel/plugin/mail_sys/cert/cocomarket.app/privkey.pem')
+    cert: fs.readFileSync('/www/server/panel/plugin/mail_sys/cert/cocomarket.app/fullchain.pem'),
+    key: fs.readFileSync('/www/server/panel/plugin/mail_sys/cert/cocomarket.app/privkey.pem')
 };
 
-const app = express();
-
-// Default route for server status
-app.get('/', (req, res) => {
-    res.json({ message: `Server is running on port ${req.secure ? HTTPS_PORT : HTTP_PORT}` });
-});
-
-// Create an HTTP server.
-http.createServer(app).listen(HTTP_PORT);
-
-// Create an HTTPS server.
-https.createServer(options, app).listen(HTTPS_PORT);
+https.createServer(options, app).listen(443);
