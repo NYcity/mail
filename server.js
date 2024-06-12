f0ce664ed2012a6058c51cc6f114eb123a93acd0
NYcity <mossei@nate.com>
2024-06-12 16:02:43 +0800

    1

--- server.js
+++ server.js
@@ -2,15 +2,28 @@ const express = require('express');
 const https = require('https');
 const fs = require('fs');
 const nodemailer = require('nodemailer');
+const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+// MAILFROM=robert@cocomarket.app
+// MAILFROM_PASSWORD=@1q2w3e4r5T
+// MAILTO=mossei@nate.com
+// SUBJECT=Test
+// BODY=Test324234321432
+// DOMAIN = cocomarket.app
 
 // Nodemailer transporter setup
 const transporter = nodemailer.createTransport({
-    service: 'mail.cocomarket.app',
+    host: process.env.DOMAIN,
+    port: process.env.PORT,
+    secure: false, // use SSL
     auth: {
-        user: 'robert@cocomarket.app',
-        pass: '@1q2w3e4r5T'
+        user: process.env.MAILFROM,
+        pass: process.env.MAILFROM_PASSWORD
     }
 });
 
@@ -21,8 +34,8 @@ app.get('/', (req, res) => {
 // Endpoint to send email
 app.get('/sendmail', (req, res) => {
     const mailOptions = {
-        from: 'robert@cocomarket.app',
-        to: 'mossei@nate.com',
+        from: process.env.MAILFROM,
+        to: process.env.MAILTO,
         subject: 'Test Email',
         text: 'This is a test email from Node.js using Nodemailer.'
     };
@@ -40,8 +53,8 @@ app.get('/sendmail', (req, res) => {
 
 // HTTPS options
 const httpsOptions = {
-    cert: fs.readFileSync('/www/server/panel/plugin/mail_sys/cert/cocomarket.app/fullchain.pem'),
-    key: fs.readFileSync('/www/server/panel/plugin/mail_sys/cert/cocomarket.app/privkey.pem')
+    cert: fs.readFileSync('/etc/letsencrypt/live/cocomarket.app/fullchain.pem'),
+    key: fs.readFileSync('/etc/letsencrypt/live/cocomarket.app/privkey.pem')
 };
 
 // Start server
